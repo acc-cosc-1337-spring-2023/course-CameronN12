b1e951f24dfb0f832c09e7776d77a9345ee1d1ae
CameronN12 <123427192+CameronN12@users.noreply.github.com>
2023-02-11 04:17:30 +0000

    Homework 2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,32 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "hwexpressions.h"
 
 //write namespace using statement for cout
-
+using std::cout; using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount = 0;
+	double tip_rate = 0;
+
+	cout<<"Enter meal amount: ";
+    cin>>meal_amount;
+
+	double tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Enter % tip: ";
+	cin>>tip_rate;
+
+	double tip_amount = get_tip_amount(meal_amount, tip_rate);
+	double total = tip_amount + tax_amount + meal_amount;
+	cout<<"\n"<<"Meal Amount: $"<<meal_amount<<"\n";
+	cout<<"Sales Tax:   $"<<tax_amount<<"\n";
+	cout<<"Tip Amount:  $"<<tip_amount<<"\n";
+	cout<<"Total:       $"<<total<<"\n";
 
 	return 0;
 }
