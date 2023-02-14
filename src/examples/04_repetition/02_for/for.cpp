501126fef81f9bb1ece211fda5eedd08ac02d4c8
CameronN12 <123427192+CameronN12@users.noreply.github.com>
2023-02-14 00:46:12 +0000

    While/for loops

--- src/examples/04_repetition/02_for/for.cpp
+++ src/examples/04_repetition/02_for/for.cpp
@@ -1,6 +1,5 @@
 #include "for.h"
-#include<iostream>
-
+using std::cout;
 //Write code for function display_numbers that accepts a number and 
 //displays every number that precedes(starting from 1) it in a line
 //Example: display_numbers(5)
@@ -14,4 +13,22 @@ RESULT:
 5
 
 */
+void display_numbers(int num)
+{
+    for(auto i=0; i < num; i++)
+    {
+       cout<<i+1<<"\n"; 
+    }
+}
 
+int sum_of_numbers_for(int num)
+{
+    auto sum = 0;
+    
+    for(auto i=0; i <= num; i++)
+    {
+        sum += i * i;
+    }
+
+    return sum;
+}
