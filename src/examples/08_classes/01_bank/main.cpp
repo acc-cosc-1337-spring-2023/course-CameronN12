dde3bdf12ff6562b0bee8309ca40904ca0ed8e4f
CameronN12 <123427192+CameronN12@users.noreply.github.com>
2023-02-28 00:54:42 +0000

    Basic classes

--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,18 @@
+#include<iostream>
+#include<time.h>
+#include "checking_account.h"
+
+using std::cout;
 
 int main()
 {
+	srand(time(NULL));
+	
+	CheckingAccount account;
+	cout<<"Balance: "<<account.get_balance()<<"\n";
+
+	CheckingAccount account1(100);
+	cout<<"Balance: "<<account1.get_balance()<<"\n";
 
 	return 0;
 }
