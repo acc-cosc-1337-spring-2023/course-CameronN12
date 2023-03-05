486838d3e3bd2fef7b9086b9ddbac419df4463d0
CameronN12 <123427192+CameronN12@users.noreply.github.com>
2023-03-05 04:43:54 +0000

    Homework 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,79 @@
-//add include statements
+#include <iostream>
+#include <string>
+using namespace std;
 
-//add function code here
+//add function(s) prototype here
+double get_gc_content(const string& dna)
+{
+   int gc = 0;
+   int strlength = dna.length();
+   for (int i = 0; i < strlength; i++)
+   {
+      if(dna[i]== 'C' || dna[i] == 'G')
+      {
+         gc++;
+      }
+   }
+   double gc_value = (double)gc / strlength;
+   return gc_value;
+}
+string reverse_string(string dna)
+{
+   int strlength = dna.length();
+   string reversedna;
+   for (int i = strlength - 1; i >= 0; i--)
+   {
+      reversedna.push_back(dna[i]);
+   }
+   return reversedna;
+}
+string get_dna_complement(string dna)
+{
+   string complement_dna;
+   string reversedna = reverse_string(dna);
+   int strlength = dna.length();
+   for (int i = 0; i < strlength; i++)
+   {
+      if (reversedna[i] == 'A')
+      {
+         complement_dna.push_back('T');
+      }
+      else if (reversedna[i] == 'T')
+      {
+         complement_dna.push_back('A');
+      }
+      else if (reversedna[i] == 'C')
+      {
+         complement_dna.push_back('G');
+      }
+      else if (reversedna[i] == 'G')
+      {
+         complement_dna.push_back('C');
+      }
+   }
+   return complement_dna;
+}
+
+int prompt_user(int option)
+{
+    int exit = 0;
+    do
+    {    
+        auto choice = 'n';
+            cout<<"Are you sure you want to exit? 'y' for yes, 'n' or no: ";
+            cin>>choice;
+            if (choice == 'y' || choice == 'Y')
+            {
+                cout<<"Goodbye!";
+                return 1;
+            }
+            else if (choice == 'n' || choice == 'N')
+                return 0;
+            else
+            {
+                cout<<"Invalid option" << "\n";
+                exit = 1;
+            }
+    }
+    while (exit == 1);
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 //add function(s) prototype here
+double get_gc_content(const string& dna);
+string reverse_string(string dna);
+string get_dna_complement(string dna);
+int prompt_user(int option);
